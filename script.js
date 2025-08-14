060cdd67bbc6abd3161ab142ac72afea2a1239d0
Adam H <183345857+adamhaouam@users.noreply.github.com>
2025-08-14 23:32:07 +1000

    add read param to addbooktolibrary function

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 const myLibrary = [];
 
-function Book(title,author, pages) { // the constructor...
+function Book(title,author, pages, read) { // Book Constructor
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
     this.title = title,
     this.author = author;
     this.pages = pages,
-    this.read = false;
+    this.read = read;
     this.id = crypto.randomUUID(),
     this.info = function() {
-        console.log(`${title} by ${author}, ${pages} pages`);
+        if (this.read == false) {
+            console.log(`${title} by ${author}, ${pages} pages, not read`);
+        }
+        else {
+            console.log(`${title} by ${author}, ${pages} pages, read`);
+        }
+        
     };
-
-    
-    // this.sayName = function() {
-    //     console.log(this.name);
-    // };
 }
 
-function addBookToLibrary(title, author, pages) {
-    let book = new Book(title, author, pages);
+function addBookToLibrary(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
     myLibrary.push(book);
-
-  // take params, create a book then store it in the array
 }
 
 
 
 
-addBookToLibrary("Book 1", "me", "20");
-addBookToLibrary("Book 2", "you", "19");
+addBookToLibrary("Book 1", "me", "20", true);
+addBookToLibrary("Book 2", "you", "19", false);
 
 for (x in myLibrary) {
-    console.log(myLibrary[x]);
     myLibrary[x].info();
 }
