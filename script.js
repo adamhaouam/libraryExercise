072539d0a725b32cba3ad017b7abbe586db065ee
Adam H <183345857+adamhaouam@users.noreply.github.com>
2025-08-14 22:45:55 +1000

    add extra properties of book class

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const myLibrary = [];
 
-function Book(name) { // the constructor...
-    this.name = name,
-    this.id = crypto.randomUUID();
-    this.sayName = function() {
-        console.log(this.name);
+function Book(title,author, pages) { // the constructor...
+    this.title = title,
+    this.author = author;
+    this.pages = pages,
+    this.read = false;
+    this.id = crypto.randomUUID(),
+    this.info = function() {
+        console.log(`${title} by ${author}, ${pages} pages`);
     };
+
+    
+    // this.sayName = function() {
+    //     console.log(this.name);
+    // };
 }
 
-function addBookToLibrary(name) {
-    let book = new Book(name);
+function addBookToLibrary(title, author, pages) {
+    let book = new Book(title, author, pages);
     myLibrary.push(book);
 
   // take params, create a book then store it in the array
 }
 
-addBookToLibrary("Book 1");
-addBookToLibrary("Book 2");
+
+
+
+addBookToLibrary("Book 1", "me", "20");
+addBookToLibrary("Book 2", "you", "19");
 
 for (x in myLibrary) {
-    console.log(myLibrary[x].id);
-    myLibrary[x].sayName();
+    console.log(myLibrary[x]);
+    myLibrary[x].info();
 }
