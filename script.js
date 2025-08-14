7c26113ee47feb76f4df3624ce83ac1eefa3e251
Adam H <183345857+adamhaouam@users.noreply.github.com>
2025-08-14 22:34:08 +1000

    print list of names of objects in array

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+const myLibrary = [];
+
+function Book(name) { // the constructor...
+    this.name = name,
+    this.sayName = function() {
+        console.log(this.name);
+    };
+}
+
+function addBookToLibrary(name) {
+    let book = new Book(name);
+    myLibrary.push(book);
+
+  // take params, create a book then store it in the array
+}
+
+addBookToLibrary("Book 1");
+addBookToLibrary("Book 2");
+
+for (x in myLibrary) {
+    //console.log(myLibrary[x].name);
+    myLibrary[x].sayName();
+}
