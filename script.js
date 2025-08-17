0a9e23ae168458d6400789b53c64b072c0e34999
Adam H <183345857+adamhaouam@users.noreply.github.com>
2025-08-17 18:51:09 +1000

    add book to grid from myLibrary array

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+"use strict"
+
+const gridBox = document.querySelector(".grid");
+const dialog = document.querySelector("dialog");
+const dialogClose = dialog.querySelector(".close");
+const dialogSave = dialog.querySelector(".save");
+const titleInput = dialog.querySelector("#title");
+const authorInput = dialog.querySelector("#author");
+const pageInput = dialog.querySelector("#pages");
+const readInput = dialog.querySelector("#read");
+const newBook = document.querySelector(".newBook");
+
 const myLibrary = [];
 
 function Book(title,author, pages, read) { // Book Constructor
@@ -11,26 +23,73 @@ function Book(title,author, pages, read) { // Book Constructor
     this.id = crypto.randomUUID(),
     this.info = function() {
         if (this.read == false) {
-            console.log(`${title} by ${author}, ${pages} pages, not read`);
+            return (`${title} by ${author}, ${pages} pages, not read`);
         }
         else {
-            console.log(`${title} by ${author}, ${pages} pages, read`);
+            return (`${title} by ${author}, ${pages} pages, read`);
         }
-        
     };
 }
 
-function addBookToLibrary(title, author, pages, read) {
+function addBookToLibrary(title, author, pages, read) { //Add book to library array
     let book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
 
+function updateShelf()  { //Update gridBox to match myLibrary array
+for (let x in myLibrary) { //Create books in grid
+const div = document.createElement("div");
+div.dataset.idNo = myLibrary[x].id;
+div.classList.add("book");
+div.textContent = myLibrary[x].info();
+
+const btn = document.createElement("button"); //Add remove button to each book
+btn.textContent = "Remove";
+btn.addEventListener("click", () => {
+console.log("Deleted!");
+//match id to array and remove
+deleteBook(div.dataset.idNo);
+btn.parentElement.remove();
+});
+div.appendChild(btn);
+gridBox.appendChild(div);
+}
+}
+
+function deleteBook(id) { //Delete book from myLibrary array with ID provided
+for (let x in myLibrary) {
+if (id == myLibrary[x].id) {
+myLibrary.splice(x, 1);
+}
+}
+}
+
+
+dialogClose.addEventListener("click", () => {
+event.preventDefault();
+dialog.close();
+});
+
+dialogSave.addEventListener("click", () => {
+event.preventDefault();
+addBookToLibrary(titleInput.value, authorInput.value, pageInput.value, readInput.value);
+gridBox.replaceChildren();
+updateShelf();
+dialog.close();
+});
+
+
+
+newBook.addEventListener("click", () => {
+dialog.showModal();
+titleInput.value = "";
+authorInput.value = "";
+pageInput.value = "";
+readInput.checked = false;
+});
 
 
 
 addBookToLibrary("Book 1", "me", "20", true);
 addBookToLibrary("Book 2", "you", "19", false);
-
-for (x in myLibrary) {
-    myLibrary[x].info();
-}
+updateShelf();
